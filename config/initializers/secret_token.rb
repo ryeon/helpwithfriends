f6eaa682916a4af201f8a8bc085499f567c2d54c
redweddings <ryeonk@gmail.com>
2014-03-16 22:34:58 -0400

    first commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helpwithfriends::Application.config.secret_key_base = '44ea60e18555739a0a3f4ed575dbeaded98e0dfa9c27686a4302271df252b45bd12f34387ecd2fb8f541b2e0fbbc766c0952799808217d73c52d0dabebc4efd4'
