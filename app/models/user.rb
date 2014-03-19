e81fb3c43249438ee62e9041815cc4ee35b99d45
redweddings <ryeonk@gmail.com>
2014-03-19 15:27:00 -0400

    Finish signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
+	before_create :create_remember_token
+
+	
 	validates :name,  presence: true, length: { maximum: 40 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -11,5 +14,19 @@ class User < ActiveRecord::Base
 
   	has_many :microposts
 	has_secure_password
+
+
+	def User.new_remember_token
+    	SecureRandom.urlsafe_base64
+  	end
+
+  	def User.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  	end
+
+  	private
+		def create_remember_token
+      		self.remember_token = User.hash(User.new_remember_token)
+   		 end
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Helpwithfriends::Application.routes.draw do
   resources :microposts
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root 'pages#home'
-  match '/signup',  to: 'users#new',     via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+
   match '/help',    to: 'pages#help',    via: 'get'
   match '/about',   to: 'pages#about',   via: 'get'
   match '/contact', to: 'pages#contact', via: 'get'
