8ac9b5e6b25b6e382b40222c38f40b90490b7657
redweddings <ryeonk@gmail.com>
2014-03-19 22:37:07 -0400

    finish user edit, update, index, and destroy

--- Gemfile
+++ Gemfile
@@ -30,6 +30,12 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
+
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
+
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,21 @@ describe User do
   it {should respond_to(:remember_token)}
   it {should be_valid}
 
+  it {should respond_to(:authenticate)}
+  it {should respond_to(:admin)}
+
+  it {should be_valid}
+  it {should_not be_admin}
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it {should be_admin}
+  end
+
   describe "remember_token" do
     before{@user.save}
     its(:remember_token) {should_not be_blank}
